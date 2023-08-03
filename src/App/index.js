323b814812e00fb82a036ac6a1719c9021a2c2cc
Eddiie09 <castanedagarciaedison@gmail.com>
2023-08-03 13:58:35 -0500

    Formulo organizacion de carpetas

---
+++ src/App/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { AppUI } from './AppUI';
+import { useLocalStorage } from './useLocalStorage';
+
+// const defaultTodos = [
+//   { text: "Curso React", completed: false },
+//   { text: "Curso Javascript", completed: false },
+//   { text: "Curso manipulacion de Arrays Js", completed: false },
+//   { text: "Curso Node.js", completed: true },
+//   { text: "Creacion pagina empresa", completed: false }
+// ];
+
+// localStorage.setItem('Organizador_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('Organizador_V1');
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
+  return (
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
